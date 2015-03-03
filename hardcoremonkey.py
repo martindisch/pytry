7556864b52b447d31d9c76808f05383e4028659a
Martin Disch <martindisch@gmail.com>
2015-03-03 15:09:39 +0100

    More information

--- hardcoremonkey.py
+++ hardcoremonkey.py
@@ -7,13 +7,18 @@ script.close()
 
 monkeyTyped = ''
 lengthRecord = 0
+strokes = 0
 
 while len(monkeyTyped) < len(text) - 1:
     monkeyTyped = monkeyTyped + (random.choice(string.ascii_lowercase + ' '))
+    strokes = strokes + 1
     if monkeyTyped[len(monkeyTyped) - 1] == text[len(monkeyTyped) - 1]:
         if len(monkeyTyped) > lengthRecord:
             print monkeyTyped
             lengthRecord = len(monkeyTyped)
     else:
         monkeyTyped = ''
+    
+    if strokes % 100000 == 0:
+        print text[0:lengthRecord] + '\nafter ' + str(strokes) + ' keystrokes'
 print "Done. I'll get myself a banana."
