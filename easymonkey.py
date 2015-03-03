e8a2833f096521688be2a94735ddb0007e877de7
Martin Disch <martindisch@gmail.com>
2015-03-03 15:15:28 +0100

    Improved information output of easymonkey

--- easymonkey.py
+++ easymonkey.py
@@ -6,12 +6,14 @@ text = script.read()
 script.close()
 
 monkeyTyped = ''
+strokes = 0
 
 while len(monkeyTyped) < len(text) - 1:
     monkeyTyped = monkeyTyped + (random.choice(string.ascii_lowercase + ' '))
+    strokes = strokes + 1
     if monkeyTyped[len(monkeyTyped) - 1] == text[len(monkeyTyped) - 1]:
-        if len(monkeyTyped) % 100 == 0:
-            print 'Monkey typed ' + str(len(monkeyTyped)) + ' characters'
+        if len(monkeyTyped) % 1000 == 0:
+            print 'Monkey typed ' + str(len(monkeyTyped)) + ' correct characters after ' + str(strokes) + ' keystrokes'
     else:
         monkeyTyped = monkeyTyped[:-1]
 print "Done. I'll get myself a banana."
