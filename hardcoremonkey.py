e5641ca21db75e2d4ee4e96e16f5231fac398e25
Martin Disch <martindisch@gmail.com>
2015-03-03 15:04:13 +0100

    Hardcore version, resetting after a failed attempt

--- hardcoremonkey.py
+++ hardcoremonkey.py
@@ -6,12 +6,14 @@ text = script.read()
 script.close()
 
 monkeyTyped = ''
+lengthRecord = 0
 
 while len(monkeyTyped) < len(text) - 1:
     monkeyTyped = monkeyTyped + (random.choice(string.ascii_lowercase + ' '))
     if monkeyTyped[len(monkeyTyped) - 1] == text[len(monkeyTyped) - 1]:
-        if len(monkeyTyped) % 100 == 0:
-            print 'Monkey typed ' + str(len(monkeyTyped)) + ' characters'
+        if len(monkeyTyped) > lengthRecord:
+            print monkeyTyped
+            lengthRecord = len(monkeyTyped)
     else:
-        monkeyTyped = monkeyTyped[:-1]
+        monkeyTyped = ''
 print "Done. I'll get myself a banana."
