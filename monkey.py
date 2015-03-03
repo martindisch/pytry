28423f2399a2d2ec96899fcbdecbe0aa432fc7f0
Martin Disch <martindisch@gmail.com>
2015-03-03 14:44:42 +0100

    Improved output

--- monkey.py
+++ monkey.py
@@ -7,11 +7,11 @@ script.close()
 
 monkeyTyped = ''
 
-while len(monkeyTyped) < len(text):
+while len(monkeyTyped) < len(text) - 1:
     monkeyTyped = monkeyTyped + (random.choice(string.ascii_lowercase + ' '))
     if monkeyTyped[len(monkeyTyped) - 1] == text[len(monkeyTyped) - 1]:
-        print monkeyTyped
+        if len(monkeyTyped) % 100 == 0:
+            print 'Monkey typed ' + str(len(monkeyTyped)) + ' characters'
     else:
         monkeyTyped = monkeyTyped[:-1]
-
 print "Done. I'll get myself a banana."
